b65b640ef7c0dc04066029553999f7921e20c177
Stephen Kent <smkent@smkent.net>
2025-03-01 23:01:40 -0800

    Add config render unit test

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import shutil
+import tempfile
+from collections.abc import Iterator
+from pathlib import Path
+
+import pytest
+
+PROJECT_DIR = Path(__file__).parent.parent
+SECRETS_YAML = """
+wifi_ssid: "Captain's Log, Stardate 802.11"
+wifi_password: deadbeef
+wifi_domain: .local
+api_encryption_key: ZGVhZGJlZWZkZWFkYmVlZmRlYWRiZWVmZGVhZGJlZQo=
+ota_password: deadbeefdeadbeef
+"""
+
+
+@pytest.fixture(autouse=True, scope="session")
+def sandbox_dir() -> Iterator[Path]:
+    with tempfile.TemporaryDirectory() as td:
+        sandbox_path = Path(td) / "repo"
+        shutil.copytree(
+            PROJECT_DIR, sandbox_path, ignore=shutil.ignore_patterns(".git")
+        )
+        with open(sandbox_path / "secrets.yaml", "w") as f:
+            f.write(SECRETS_YAML)
+        yield sandbox_path
---
+++ tests/test_boards.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+from pathlib import Path
+
+import pytest
+
+from .conftest import PROJECT_DIR
+
+
+@pytest.mark.parametrize(
+    "board_file_path",
+    (
+        pytest.param(testfn, id=str(testfn))
+        for testfn in [
+            fn.relative_to(PROJECT_DIR)
+            for fn in (PROJECT_DIR / "boards").rglob("*.yaml")
+        ]
+    ),
+)
+def test_render_board_config(board_file_path: Path, sandbox_dir: Path) -> None:
+    for fn in (sandbox_dir / "boards").rglob("*.yaml"):
+        device_temp_file = sandbox_dir / "device.yaml"
+        try:
+            with open(device_temp_file, "w") as f:
+                print(
+                    f"packages:{os.linesep}"
+                    f"  board: !include {board_file_path}",
+                    file=f,
+                )
+            subprocess.run(
+                ["esphome", "config", str(device_temp_file)], cwd=sandbox_dir
+            )
+        finally:
+            if device_temp_file.exists():
+                device_temp_file.unlink()
